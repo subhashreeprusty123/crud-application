b66d977a4fffb0448b48e32e0a5dbf475b4704f4
Subhashree Prusty <subhashreeprusty200@gmail.com>
2023-05-04 23:19:38 +0530

    modified db.js

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 const Connection = async (username,password) => {
-    const URL = `mongodb+srv://${username}:${password}@crud-app.lurzwtu.mongodb.net/?retryWrites=true&w=majority`;
+    const URL = `mongodb://${username}:${password}@ac-au5lcme-shard-00-00.lurzwtu.mongodb.net:27017,ac-au5lcme-shard-00-01.lurzwtu.mongodb.net:27017,ac-au5lcme-shard-00-02.lurzwtu.mongodb.net:27017/CRUD-APPLICATION?ssl=true&replicaSet=atlas-qoxmgp-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try{
         await mongoose.connect(URL,{useUnifiedTopology: true, useNewUrlParser: true});
         console.log('Database connected successfully');
